30a0e52424a16b24ae21f3558b4c456bc6dd441d
TenYeshi <yeshileksang675@gmail.com>
2025-05-14 17:17:54 +0530

    main

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from app import app
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5050, debug=True)
